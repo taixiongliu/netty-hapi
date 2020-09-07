41ae3ac344bed066a02d639b6f8b75e67a4b9b80
liutaixiong <531457277@qq.com>
2020-09-07 15:34:31 +0800

    update fastjson version. add DOMParser method about bean list parse.

--- src/main/java/com/github/taixiongliu/hapi/dom/DOMParser.java
+++ src/main/java/com/github/taixiongliu/hapi/dom/DOMParser.java
@@ -2,7 +2,9 @@ package com.github.taixiongliu.hapi.dom;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -94,5 +96,47 @@ public class DOMParser {
         } 
         
         return map;
+	}
+	
+	/**
+	 * 
+	 * @param fileName : XML file name
+	 * @return XML {@code <K,V>} nodeName(key) and textContent(value)
+	 * <br><br>
+	 * <b>map empty:parse error or file not exists</b>
+	 */
+	public List<Map<String, String>> parseListMap(String fileName){
+		List<Map<String, String>> maps = new ArrayList<Map<String,String>>();
+        //parse result
+		Document document = parse(fileName);
+		//if parse error or file not exists
+		if(document == null){
+			return maps;
+		}
+        //root element 
+        Element rootElement = document.getDocumentElement(); 
+ 
+        //traverse bean list
+        NodeList nodes = rootElement.getChildNodes(); 
+        for (int i=0; i < nodes.getLength(); i++) { 
+           Node node = nodes.item(i); 
+           if (node.getNodeType() == Node.ELEMENT_NODE) {   
+              Element child = (Element) node; 
+              Map<String, String> map = new HashMap<String, String>();
+              NodeList bean = child.getChildNodes();
+              //traverse child elements
+              for(int j = 0; j < bean.getLength(); j ++){
+            	  Node nd = bean.item(j); 
+                  if (nd.getNodeType() == Node.ELEMENT_NODE) {
+                	  Element cd = (Element) nd;
+                	  //cache child element 
+                	  map.put(cd.getNodeName(), cd.getTextContent());
+                  }
+              }
+              maps.add(map);
+           } 
+        } 
+        
+        return maps;
 	}
 }
